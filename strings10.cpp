7ef866955cf434f4afb0fd5ee053c3593387664d
samwanyua <samexample8@gmail.com>
2024-02-28 17:43:46 +0300

    Removing text from an existing code

---
+++ strings10.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>  //string library
+
+using namespace std;
+
+int main(){
+    //Removing specific text from a string
+    string um = "These um are um my um words and um I want um us to stop um talking um like this";
+
+    string target = " um ";
+
+    int found = -1;
+    do{
+        found = um.find(target, found+1);
+        if(found != -1){
+            um = um.substr(0, found) + " " +  um.substr(found + target.length());
+        }
+    }while(found != -1);
+
+    cout << um << endl;
+    return 0;
+}

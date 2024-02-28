f23a8eae6e1f7fae6565263fa3001272e55f680d
samwanyua <samexample8@gmail.com>
2024-02-28 12:23:28 +0300

    Length

---
+++ strings.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>  //string library
+
+using namespace std;
+
+int main(){
+
+    string s = "some text";
+    //length
+    cout << s.length()<< endl;
+
+    return 0;
+}

9be06b6cbb9e77ed79432de941025cebb6746ba8
samwanyua <samexample8@gmail.com>
2024-02-28 12:36:24 +0300

    Resize

--- strings.cpp
+++ strings.cpp
@@ -9,5 +9,26 @@ int main(){
     //length
     cout << s.length()<< endl;
 
+    //resize
+    string text = "some words";
+
+    cout << text << endl;
+    // text.resize(6);
+    cout << text << endl;
+    // text.resize(20);
+    cout << text.length() << endl; //20
+    cout << (int)text[12] << endl; //0 - Null
+    
+    text.resize(20, '!'); //some words!!!!!!!!!!
+    cout << text << endl;
+
+    string word;
+    cout << "Enter a word: " << endl;
+    getline(cin, word);
+
+    word.resize(word.length()+3, '!');
+    cout << word << endl;
+
+
     return 0;
 }
